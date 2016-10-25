401fd173638ec4a8d9c8f96287c20578146ced1f
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2016-10-25 14:47:30 -0200

    new models

--- restAPI/models.py
+++ restAPI/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from mongoengine import *
 
-class information(Document):
+class Passenger(Document):
+	nome = StringField(required=True)
+	sobrenome = StringField(required=True)
+	data_nascimento = DateTimeField(auto_now=False)
+	documento = StringField(required=True)
+	nacionalidade = StringField(required=True)
+	tag = ListField(EmbeddedDocumentField(Tag))
+	bagagens = ListField(EmbeddedDocumentField(Bagagem))
+
+class Bagagem(EmbeddedDocument):
+	documento = StringField(required=True)
+	peso = StringField(required=True)
+	tag = ListField(EmbeddedDocumentField(Tag))
+
+class Tag(EmbeddedDocument):
     tag_id = StringField(required=True)
+    date_time = DateTimeField(auto_now=True)
+
+class Leitura(Document):
+	tag_id = StringField(required=True)
     sender_id = StringField(required=True)
     date_time = DateTimeField(auto_now=True)
