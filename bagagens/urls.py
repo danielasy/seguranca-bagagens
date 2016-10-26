e0e6b111c3d78841ac38a0400108c945a0d96903
Hugo Possani <hugopossani@gmail.com>
2016-10-26 13:18:43 -0200

    Criacao do layout do site

--- bagagens/urls.py
+++ bagagens/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', include('website.urls')),
     url(r'^', include('restAPI.urls')),
 ]
---
+++ website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from datetime import datetime
+
+@csrf_exempt
+def index(request):
+    return render(request, 'index.html', {})
